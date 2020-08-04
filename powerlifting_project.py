6870b3c74ea925169acd29c3eaf7f064e1471ec0
killsparx <56236800+killsparx@users.noreply.github.com>
2020-08-04 12:55:05 -0700

    Add files via upload

---
+++ powerlifting_project.py
@@ -0,0 +1,17 @@
+# Ask the weight in Pounds and convert to KG, rounded up.
+lbs = input("What is the weight in pounds?")
+kg = (float(lbs) / 2.2046)
+weight = round(kg)
+print(str(weight) + " kg")
+# Variables for the bar loading
+red_plate = 25
+blue_plate = 20
+yellow_plate = 15
+green_plate = 10
+white_plate = 5
+small_red_plate = 2.5
+small_blue_plate = 2
+small_yellow_plate = 1.5
+small_green_plate = 1
+small_white_plate = 0.5
+# Function for doing the maths
